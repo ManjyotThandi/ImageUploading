2b2531ea103fcbf8e56ade85e01ce5e242556253
Manjyot Thandi <manjyot.thandi@gmail.com>
2020-08-27 15:22:31 -0400

    Added FileStore, UserProfile and The User profile fake store

---
+++ aws-image-upload/src/main/java/com/amigoscode/awsimageupload/datastore/FakeUserProfileStore.java
@@ -0,0 +1,26 @@
+package com.amigoscode.awsimageupload.datastore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import com.amigoscode.awsimageupload.profile.UserProfile;
+
+@Repository
+public class FakeUserProfileStore {
+
+	private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
+
+	// When the class is loaded this static block will run
+	static {
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "Manjyot", null));
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "Keen", null));
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "Track", null));
+	}
+
+	public List<UserProfile> getUserProfile() {
+		return USER_PROFILES;
+	}
+}
--- aws-image-upload/src/main/java/com/amigoscode/awsimageupload/filestore/FileStore.java
+++ aws-image-upload/src/main/java/com/amigoscode/awsimageupload/filestore/FileStore.java
@@ -7,12 +7,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @Service
 public class FileStore {
 
+	// Spring will look for a bean that returns AmazonS3
 	private final AmazonS3 s3;
 
 	// You can either put @Autowired on the constructor or on the field. When used
@@ -25,15 +27,25 @@ public class FileStore {
 
 	public void save(String path, String fileName, Optional<Map<String, String>> optionalMetadata,
 			InputStream inputStream) {
-		
+
 		ObjectMetadata metadata = new ObjectMetadata();
-		
+
+		// if the optional map is present take each value in the map and perform
+		// metadata.addUserMetadata(k,v)
 		optionalMetadata.ifPresent(map -> {
-			if(!map.isEmpty()) {
-				map.forEach(action);
+			if (!map.isEmpty()) {
+				map.forEach((k, v) -> {
+					// We can also use method reference here instead of lambda
+					metadata.addUserMetadata(k, v);
+				});
 			}
 		});
+
 		// uploads a new object to the bucket
-		s3.putObject(path, fileName, inputStream, metadata);
+		try {
+			s3.putObject(path, fileName, inputStream, metadata);
+		} catch (AmazonServiceException e) {
+			throw new IllegalStateException("Failed to store file to s3", e);
+		}
 	}
 }
